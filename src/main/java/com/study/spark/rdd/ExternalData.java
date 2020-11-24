e11225e1475ef8f6a07de421a53a8d5d99d69e6e
guanjun <guanjun@kingsoft.com>
2020-11-24 19:25:19 +0800

    内部数据

--- src/main/java/com/study/spark/rdd/ExternalData.java
+++ src/main/java/com/study/spark/rdd/ExternalData.java
@@ -5,25 +5,19 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
-
 import java.util.Arrays;
-import java.util.List;
 
-public class Join {
+
+public class ExternalData {
 
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName("Join").setMaster("local[2]");
+        SparkConf conf = new SparkConf().setAppName("ExternalData").setMaster("local[2]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> distFile = sc.textFile("D:\\gitRepository\\spark-study\\src\\main\\resources\\word-count.txt");
         JavaPairRDD<String, Integer> javaPairRDD =
                 distFile.flatMap(s -> Arrays.asList(s.split(" ")).iterator())
                         .mapToPair(s -> new Tuple2(s, 1));
-
-        List<String> list = Arrays.asList("Are", "you", "our");
-        JavaPairRDD<String, Integer> other =
-                sc.parallelize(list).mapToPair(s -> new Tuple2<>(s, 1));
-
-        System.out.println(javaPairRDD.join(other).collect());
+        System.out.println(javaPairRDD.collect());
     }
 }
