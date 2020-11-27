30c11fcc53e6376bb74be47659745f1c5bfbbe58
guanjun <guanjun@kingsoft.com>
2020-11-27 18:04:50 +0800

    分区

--- src/main/java/com/study/spark/streaming/WordCount.java
+++ src/main/java/com/study/spark/streaming/WordCount.java
@@ -1,6 +1,7 @@
 package com.study.spark.streaming;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
@@ -8,7 +9,9 @@ import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import scala.Tuple2;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Queue;
 
 /**
  * spark-submit --class com.study.spark.streaming.WordCount spark-study-1.0-SNAPSHOT.jar
@@ -18,15 +21,16 @@ public class WordCount {
         SparkConf conf = new SparkConf();
         conf.setAppName("wordCount").setMaster("local[3]");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaStreamingContext ssc = new JavaStreamingContext(sc, Durations.seconds(5));
+        JavaStreamingContext ssc = new JavaStreamingContext(sc, Durations.seconds(120));
+        JavaRDD<String> javaRDD1 = sc.parallelize(Arrays.asList("111", "222", "333"));
+        JavaRDD<String> javaRDD2 = sc.parallelize(Arrays.asList("111", "222", "444"));
+        Queue<JavaRDD<String>> queue = new ArrayDeque<>();
+        queue.add(javaRDD1);
+        queue.add(javaRDD2);
+        JavaDStream<String> lines = ssc.queueStream(queue);
 
-        JavaDStream<String> lines = ssc.socketTextStream("localhost", 8800);
-
-        JavaPairDStream<String, Integer> wordCounts = lines.flatMap(s -> Arrays.asList(s.split(" ")).iterator())
-                .mapToPair(s -> new Tuple2<>(s, 1)).reduceByKey((a, b) -> a + b);
-        wordCounts.foreachRDD(rdd->{
-            rdd.saveAsTextFile("E:/project/sparkstudy/"+rdd.name());
-        });
+        JavaPairDStream<String, Integer> wordCounts = lines.mapToPair(s -> new Tuple2<>(s, 1)).reduceByKey((a, b) -> a + b);
+        wordCounts.foreachRDD(rdd -> System.out.println(rdd.collect()));
         ssc.start();
         ssc.awaitTermination();
     }
