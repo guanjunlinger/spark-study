b7813db6b00b8e8ab215777d2682ded48440219d
guanjun <1126964622@qq.com>
2020-11-29 20:51:34 +0800

    分区

--- src/main/java/com/study/spark/streaming/WordCount.java
+++ src/main/java/com/study/spark/streaming/WordCount.java
@@ -9,8 +9,8 @@ import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import scala.Tuple2;
 
-import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Queue;
 
 /**
@@ -21,12 +21,11 @@ public class WordCount {
         SparkConf conf = new SparkConf();
         conf.setAppName("wordCount").setMaster("local[3]");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaStreamingContext ssc = new JavaStreamingContext(sc, Durations.seconds(120));
-        JavaRDD<String> javaRDD1 = sc.parallelize(Arrays.asList("111", "222", "333"));
-        JavaRDD<String> javaRDD2 = sc.parallelize(Arrays.asList("111", "222", "444"));
-        Queue<JavaRDD<String>> queue = new ArrayDeque<>();
-        queue.add(javaRDD1);
-        queue.add(javaRDD2);
+        JavaStreamingContext ssc = new JavaStreamingContext(sc, Durations.seconds(60));
+        JavaRDD<String> javaRDD = sc.parallelize(Arrays.asList("111", "222", "333"
+                , "111", "222"));
+        Queue<JavaRDD<String>> queue = new LinkedList<>();
+        queue.add(javaRDD);
         JavaDStream<String> lines = ssc.queueStream(queue);
 
         JavaPairDStream<String, Integer> wordCounts = lines.mapToPair(s -> new Tuple2<>(s, 1)).reduceByKey((a, b) -> a + b);
