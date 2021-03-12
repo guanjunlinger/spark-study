ad5512efc01e07bf2de5c252abeae63cfd2f66bb
guanjun <guanjun@kingsoft.com>
2021-03-12 18:55:08 +0800

    DataFrame datasource

---
+++ src/main/java/com/study/spark/sql/function/ScalarFunction.java
@@ -0,0 +1,32 @@
+package com.study.spark.sql.function;
+
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.api.java.UDF1;
+import org.apache.spark.sql.expressions.UserDefinedFunction;
+import org.apache.spark.sql.types.DataTypes;
+
+import static org.apache.spark.sql.functions.udf;
+
+public class ScalarFunction {
+
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Java Spark SQL UDF scalar ")
+                .getOrCreate();
+
+        UserDefinedFunction strLen = udf(new StrLenFunction(), DataTypes.IntegerType);
+
+
+        spark.udf().register("strLen", strLen);
+        spark.sql("SELECT strLen('test')").show();
+    }
+
+    private static class StrLenFunction implements UDF1<String, Integer> {
+        @Override
+        public Integer call(String s) {
+            return s.length();
+        }
+    }
+
+}
